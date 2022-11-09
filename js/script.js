21cfd838681804df35f6c87a54ed9fe21a223d22
aCute5 <costantini95dario@gmail.com>
2022-11-09 10:34:27 +0100

    carousel added with create element

--- js/script.js
+++ js/script.js
@@ -1,11 +1,11 @@
 const arrImages = [
 	{
-		image:"01.webp",
+		image: '01.webp',
 		title: "Marvel's Spiderman Miles Morale",
 		text: 'Experience the rise of Miles Morales as the new hero masters incredible, explosive new powers to become his own Spider-Man.',
 	},
 	{
-		image:'02.webp',
+		image: '02.webp',
 		title: 'Ratchet & Clank: Rift Apart',
 		text: 'Go dimension-hopping with Ratchet and Clank as they take on an evil emperor from another reality.',
 	},
@@ -24,47 +24,94 @@ const arrImages = [
 		title: "Marvel's Avengers",
 		text: "Marvel's Avengers is an epic, third-person, action-adventure game that combines an original, cinematic story with single-player and co-operative gameplay.",
 	},
-]
+];
 
+const eleSliderViewer = document.querySelector('.slider-viewer');
+const eleSliderThumbs = document.querySelector('.thumbs');
+const eleBtnLeft = document.querySelector('.btn-left');
+const eleBtnRight = document.querySelector('.btn-right');
 
-const eleContainer = document.querySelector(".container-blur") //Appendo al container con innerHtml la mia card
+// creare i tag immagine nell'html
+for (let i = 0; i < arrImages.length; i++) {
+	// creare le slide
+	// creare il div .slide
+	const eleSlide = document.createElement('div'); // <div></div>
+	eleSlide.classList.add('slide'); // <div class="slide"></div>
+	// creare elemento immagine
+	const eleImg = document.createElement('img'); // <img>
+	eleImg.src = 'img/' + arrImages[i].image; // <img src="img/nomeImmagine_qui">
+	eleImg.alt = arrImages[i].title; // <img src="img/nomeImmagine_qui" alt="titolo_qui">
+	// creare il div .text
+	const eleText = document.createElement('div'); // <div></div>
+	eleText.classList.add('text'); // <div class="text"></div>
+	eleText.innerHTML = `
+		<h2>${arrImages[i].title}</h2>
+		<p>${arrImages[i].text}</p>
+	`;  // <div class="text"> <h2>titolo_qui</h2> <p>text_qui</p> </div>
 
+	eleSlide.append(eleImg); // <div class="slide"> <img src="img/nomeImmagine_qui" alt="titolo_qui"> </div>
+	eleSlide.append(eleText);
+	/*
+	<div class="slide">
+		<img src="img/nomeImmagine_qui" alt="titolo_qui">
+		<div class="text">
+			<h2>titolo_qui</h2>
+			<p>text_qui</p>
+		</div>
+	</div>
+	*/
 
-				   
-for (let i = 0; i < arrImages.length; i++) { 
-	eleContainer.innerHTML +=
-                     `
-					   <div class="slider"> 
-					   <div class="slider-viewer">
-						   <img src="img/${arrImages[i].image}" class = "slider-img currentimage" ">
-						   <div class="info">
-							   <h4>${arrImages[i].title}</h4> 
-							   <span>${arrImages[i].text}</span> 
-						   </div>
-					   </div>
-					   <div class="thumbs">
-						   <div class="controls">
-							   <div class="btn-left">&lt;</div>
-							   <div class="btn-right">&gt;</div>
-						   </div>
-					   </div>
-				   </div>
-				   `
-const eleSlider = document.querySelector(".slider")
-const eleImg = document.querySelector("#currentimage")
-const eleInfo = document.querySelector(".info")
 	if (i === 0) {
-		eleSlider.classList.add("active")
-}
-	const eleSliderThumbs = document.querySelector(".thumbs")
-	const eleThumb = document.createElement("img")
-	eleThumb.classList.add("currentimage")
+		eleSlide.classList.add('active');
+	}
+	eleSliderViewer.append(eleSlide);
+
+	// creare i tag immagine che vanno nella sezione .thumbs
+	const eleThumb = document.createElement('img');
+	eleThumb.src = 'img/' + arrImages[i].image;
 	eleThumb.classList.add('thumb-img');
-	eleThumb.src = `"img/${arrImages[i].image}"`
 	if (i === 0) {
 		eleThumb.classList.add('active');
 	}
 	eleSliderThumbs.append(eleThumb);
-
-	
 }
+// document.querySelector('.slider-img').classList.add('active');
+
+const listEleImg = document.querySelectorAll('.slide'); // non e' un array ma qualcosa di simile
+const listThumbs = document.querySelectorAll('.thumb-img');
+
+let activeIndex = 0;
+document.body.style.backgroundImage = `url('img/${arrImages[activeIndex].image}')`;
+
+// aggiungere gli event listeners ai due bottoni
+eleBtnRight.addEventListener('click', function () {
+	// togliere la classe active dall'elemento attivo corrente
+	listEleImg[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+
+	activeIndex++;
+	if (activeIndex === listEleImg.length) {
+		activeIndex = 0;
+	}
+
+	// aggiungere la classe active all'elemento successivo
+	listEleImg[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+	document.body.style.backgroundImage = `url('img/${arrImages[activeIndex].image}')`;
+});
+
+eleBtnLeft.addEventListener('click', function () {
+	// togliere la classe active dall'elemento attivo corrente
+	listEleImg[activeIndex].classList.remove('active');
+	listThumbs[activeIndex].classList.remove('active');
+
+	if (activeIndex === 0) {
+		activeIndex = listEleImg.length;
+	}
+	activeIndex--;
+
+	// aggiungere la classe active all'elemento successivo
+	listEleImg[activeIndex].classList.add('active');
+	listThumbs[activeIndex].classList.add('active');
+	document.body.style.backgroundImage = `url('img/${arrImages[activeIndex].image}')`;
+});
